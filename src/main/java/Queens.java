e2904e752e7775285b8340ae0c6c9cd799eeb655
Jesse Bartola <jrbartola@gmail.com>
2017-01-05 23:37:59 -0500

    Initial commit

---
+++ src/main/java/Queens.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Jesse on 1/5/2017.
+ */
+public class Queens {
+
+    private char[][] matrix;
+
+    public Queens(int dim) {
+        matrix = new char[dim][dim];
+    }
+
+    public static void main(String[] args) {
+        Queens eightQueen = new Queens(8);
+        
+    }
+}
