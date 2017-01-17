25421a4c6ec363843a6046d536f4c592e4ddd3e4
Jesse Bartola <jrbartola@gmail.com>
2017-01-17 15:26:03 -0500

    Documentation on nQueens solver

--- src/main/java/QueensBoard.java
+++ src/main/java/QueensBoard.java
@@ -1,6 +1,3 @@
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
 
 /**
  * This class is used as an implementation of an n x n
@@ -16,6 +13,17 @@ public class QueensBoard extends Queens {
         this.dim = dim;
     }
 
+
+    /** Backtracking approach to solve the nQueens problem
+     *
+     *  Recursively place queens on the board, checking if each
+     *  placement is valid. If we run into an invalid placement
+     *  remove the previous queen from the board and continue placing
+     *  queens until we find a successful configuration
+     *
+     * @param col The column to start at
+     * @return A completed nQueens board
+     */
     public Queens nQueens(int col) {
         if (count == dim)  {
             return this;
@@ -31,16 +39,29 @@ public class QueensBoard extends Queens {
                     return fin;
                 }
 
+                // Backtrack if our placement was invalid
                 removeQueen(r, col);
             }
         }
 
+        // If we can't find a valid queen placement
+        // for this column return null
         return null;
     }
 
     public static void main(String[] args) {
-        QueensBoard eightQueen = new QueensBoard(8);
-        System.out.println(eightQueen.nQueens(0));
+        QueensBoard nQueensBoard;
+
+        if (args.length != 0) {
+            int numOfQueens = Integer.parseInt(args[0]);
+            nQueensBoard = new QueensBoard(numOfQueens);
+
+        } else {
+            nQueensBoard = new QueensBoard(8);
+        }
+
+        System.out.println(nQueensBoard.nQueens(0));
+
     }
 
 }
